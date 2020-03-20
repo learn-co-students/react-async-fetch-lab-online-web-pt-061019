100de48d32167490c84bffb61fae60c467d38b30
Sam Markham <sam.e.markham@gmail.com>
2020-03-20 11:00:24 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      people: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+    .then(res => res.json())
+    .then(people => {
+      console.log(people)
+      this.setState({
+        people: people
+      })
+    })
+  }
+
+  // displayPeople = () => {
+  //   return this.state.people.map(person => {
+  //     console.log(person.name)
+  //     return person.name
+  //   })
+  // }
+
+  render() {
+    return (
+      <div>
+        {/* {this.displayPeople()} */}
+      </div>
+    );
+  }
+}
+
+export default App;

53605839a4037d71c749abee4da8722daa805142
Daniel  Bovington <dbovington@macbook-pro-2.home>
2020-03-23 18:42:07 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    state = {
+        astronauts: []
+    }
+
+    componentDidMount(){
+        const astros = fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(({people}) => this.setState({astronauts: people}))
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.astronauts.map(a => <p key={a.name}>{a.name}</p>)}
+            </div>
+        )
+    }
+}
